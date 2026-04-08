2122de8287b03e1ae09068ab561308b2db9ea5c4
raghava3333 <157042527+raghava3333@users.noreply.github.com>
2026-04-08 19:32:56 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+import io
+
+app = FastAPI()
+
+# Load model
+model = torch.load("full_model_eff.pth", map_location='cpu')
+model.eval()
+
+class_names = [
+    'Anthracnose fruit',
+    'Anthracnose leaf',
+    'Bacterial Canker fruit',
+    'Gall_Mid leaf',
+    'Healthy fruit',
+    'Healthy leaf'
+]
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    img_tensor = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(img_tensor)
+        probs = torch.nn.functional.softmax(output[0], dim=0)
+
+    predicted_idx = torch.argmax(probs).item()
+    confidence = probs[predicted_idx].item()
+    label = class_names[predicted_idx]
+
+    return {
+        "disease": label,
+        "confidence": f"{confidence*100:.2f}%"
+    }
